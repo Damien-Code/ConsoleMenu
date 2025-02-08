c8caabbdb9457f00ff5de4d962c6585e6880ae53
Damien-Code <dovandenijssel@live.nl>
2025-02-08 19:17:24 +0100

    Extra exercise 5

---
+++ Configuration Data/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Configuration_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationData config1 = new ConfigurationData("extra exercise 5", 12, 150);
+            Console.WriteLine($"{config1.ApplicationName} {config1.Version} {config1.MaxUsers}");
+        }
+    }
+
+    class ConfigurationData
+    {
+        private string _applicationName;
+        private int _version;
+        private int _maxUsers;
+
+        public string ApplicationName { get => _applicationName; }
+        public int Version { get => _version; }
+        public int MaxUsers { get => _maxUsers; }
+
+        public ConfigurationData()
+        {
+            _applicationName = "Extra Exercise";
+            _version = 1;
+            _maxUsers = 100;
+        }
+
+        public ConfigurationData(string applicationName, int version, int maxUsers)
+        {
+            _applicationName = applicationName;
+            _version = version;
+            _maxUsers = maxUsers;
+        }
+    }
+
+}
